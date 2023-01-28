9800bf1bfb977f8b55150e96159bbe66392a5ffe
miguetech16 <113340616+miguetech16@users.noreply.github.com>
2023-01-28 13:20:04 +0000

    Add files via upload
    Kata 5

---
+++ kata5/Kata5.java
@@ -0,0 +1,22 @@
+package kata5;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata5 {
+
+    public static void main(String[] args) throws SQLException {
+        FlightStore store = new SqliteFlightStore(new File("flights.db"));
+        Map<Integer,Integer> histogram = new HashMap<>();
+        for (Flight flight : store.flights()) {
+            int arrivalDelay = (flight.getArrivalDelay() / 5) * 5;
+            histogram.put(arrivalDelay, histogram.getOrDefault(arrivalDelay,0)+1);            
+        }
+        for (int delay : histogram.keySet())
+            System.out.println(delay + ": " + histogram.get(delay));
+    }
+    
+}
